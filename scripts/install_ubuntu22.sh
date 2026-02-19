2d5107cc58c57fd9a7c2d991578b8f1a4c1fa6ae
junchao <junchaoj@gmail.com>
2026-02-19 20:11:26 +0800

    improved

--- scripts/install_ubuntu22.sh
+++ scripts/install_ubuntu22.sh
@@ -7,6 +7,13 @@ sudo apt install -y \
   libtbb12 libtbbmalloc2 \
   libunwind8
 
+sudo apt install -y \
+  libxcb-cursor0 \
+  libxcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-xinerama0 \
+  libxkbcommon-x11-0 \
+  libxrender1 libxi6 libxext6 libx11-6 \
+  libgl1
+
 # 创建 venv
 python3 -m venv .venv
 source .venv/bin/activate
