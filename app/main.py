566de6bd8d9754b6b27b4c7df779b200691e87e3
junchao <junchaoj@gmail.com>
2026-02-20 19:31:20 +0800

    improved

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from ui.main_window import MainWindow
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
-
+    
 def main():
     app = QApplication(sys.argv)
     w = MainWindow()
