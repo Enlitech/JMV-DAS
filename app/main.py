968f7be78dfc3f857261d150b3d47a420cddd21d
junchao <junchaoj@gmail.com>
2026-02-20 19:32:50 +0800

    improved

--- app/main.py
+++ app/main.py
@@ -1,14 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication
-import os
 
 from ui.main_window import MainWindow
 
-# Ensure project root is on sys.path so we can import `backend.*`
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if ROOT_DIR not in sys.path:
-    sys.path.insert(0, ROOT_DIR)
-    
 def main():
     app = QApplication(sys.argv)
     w = MainWindow()
